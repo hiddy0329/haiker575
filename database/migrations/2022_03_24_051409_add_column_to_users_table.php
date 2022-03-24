a5b817b9a0c42da4c4c2b8694ccec3247089d24c
hiddy0329 <hiddy19970329@gmail.com>
2022-03-24 14:32:11 +0900

    usersテーブルにカラムを追加

---
+++ database/migrations/2022_03_24_051409_add_column_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //追加するカラムを記述する
+            $table->longText('profile');  // プロフィール
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //削除するカラムを記述する
+            $table->dropColumn('profile');  // プロフィール
+        });
+    }
+}
