327aaefc02f1644c6df00ef1af9b76e2f7a51913
hiddy0329 <hiddy19970329@gmail.com>
2022-03-25 19:47:12 +0900

    参照するテンプレートファイルの変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,9 +115,9 @@
                         </ul>
                     </div>
                 </nav>
-                <!-- <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+                <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
                     <h1 class="h2">Dashboard</h1>
-                </main> -->
+                </main>
             </div>
         </div>
 
