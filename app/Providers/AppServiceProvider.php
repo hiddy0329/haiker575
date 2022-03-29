821c8eef07dde81bee9ff9eba4331b5d105010a5
hiddy0329 <hiddy19970329@gmail.com>
2022-03-29 13:45:25 +0900

    Varchar型の文字数を191に制限

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    public function boot()
+    {
+        //以下を記述
+        Schema::defaultStringLength(191);
     }
 }
