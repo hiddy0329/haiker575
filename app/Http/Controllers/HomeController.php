7be9e7028eee53576d2fc2dc97b6ad4d41e30c80
hiddy0329 <hiddy19970329@gmail.com>
2022-03-29 19:57:07 +0900

    データ保存用のstoreメソッドを作成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -33,4 +34,20 @@ class HomeController extends Controller
         // compactにユーザーデータを入れて渡す
         return view('create', compact('user'));
     }
+
+    public function store(Request $request)
+    {   
+        // $requestとしてhtmlから投げられたデータを全て$dataに代入する
+        $data = $request->all();
+        // dd($data);
+        // 送信されたデータをDB（memosテーブル）に挿入
+        // POSTモデルにDBへ保存する命令を出す
+        $post_id = Post::insertGetId([
+            'ku' => $data['ku'], 'description' => $data['description'], 'user_id' => $data['user_id'], 'status' => 1
+        ]);
+        
+        // リダイレクト処理
+        return redirect()->route('home');
+    }
+    
 }
