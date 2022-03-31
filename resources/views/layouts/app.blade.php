11f397c979f855b636d99e61d14dc182241c2593
hiddy0329 <hiddy19970329@gmail.com>
2022-03-31 21:10:38 +0900

    一覧ビューの整形

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
                         </ul>
             </div>
         </nav>
-        <div class="col-md-5 ml-auto px-0">
+        <div class="col-md-5 ml-auto px-0 ichiran">
             <div class="card h-100">
                 <div class="card-header d-flex">
                     投句一覧 <a class='ml-auto' href='/create'><i class="fas fa-plus-circle"></i></a>
@@ -122,7 +122,7 @@
                             <h1 class="ku">{{ $post['ku'] }}</h2>
                         </div>
                         <p class="description p-3">{{ $post['description'] }}</p>
-                        作者：<a href="#" class="stretched-link">{{ $post->user->name }}</a>
+                        <p class="px-3">作者：<a href="#" class="stretched-link">{{ $post->user->name }}</a></p>
                     </div>
                 </div>
         @endforeach
