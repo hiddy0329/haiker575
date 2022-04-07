ef81f2f7cf192aebf3a3b1549106f4cc6656616d
hiddy0329 <hiddy19970329@gmail.com>
2022-04-07 19:47:31 +0900

    commentsコントローラーのstoreメソッドへのルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/edit/{id}', 'HomeController@edit')->name('edit');
 Route::post('/update/{id}', 'HomeController@update')->name('update');
 Route::get('/show/{id}', 'HomeController@show')->name('show');
 Route::post('/delete/{id}', 'HomeController@delete')->name('delete');
+
+Route::post('/posts/{post_id}/comments','CommentsController@store');
