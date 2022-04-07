78b74b99c0b5fee0f26c2e249fcf92b642770da0
hiddy0329 <hiddy19970329@gmail.com>
2022-04-07 16:10:41 +0900

    showメソッドを作成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,6 +74,17 @@ class HomeController extends Controller
         Post::where('id', $id)->update(['ku' => $inputs['ku'], 'description' => $inputs['description'] ]);
         return redirect()->route('home');
     }
+
+    public function show($id){
+        // 該当するIDの句をデータベースから取得
+        $user = \Auth::user();
+        // ステータスが1かつ送られてきたidがデータベースのpost_idと一致するものかつログインしているユーザーのものを取得する
+        $post = Post::where('status', 1)->where('id', $id)->where('user_id', $user['id'])->first();
+        // dd($post);
+        $posts = Post::where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        //取得した句をViewに渡す
+        return view('show',compact('post', 'user', 'posts'));
+    }
     
     public function delete(Request $request, $id)
     {   
