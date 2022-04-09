d27fb7c8189492e322d2c1f0571601a3c122d561
hiddy0329 <hiddy19970329@gmail.com>
2022-04-09 16:49:03 +0900

    リンクを作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
                         </a>
                     </li>
                     <li class="nav-item h1">
-                        <a class="nav-link" href="#">
+                        <a class="nav-link" href="/users/{{ Auth::id() }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                             <span class="ml-2">マイページ</span>
                         </a>
@@ -119,7 +119,7 @@
                             <h1 class="ku">{{ $post['ku'] }}</h2>
                         </div>
                         <p class="description p-3">{{ $post['description'] }}</p>
-                        <p class="px-3">作者：{{ $post->user->name }}</p>
+                        <p class="px-3">作者：<a href="/users/{{ $post->user_id }}" class='px-3'>{{ $post->user->name }}</a></p>
                         @if (Auth::id() == $post->user_id)
                             <a href="/edit/{{ $post['id'] }}" class='px-3'>推敲</a>
                         @endif
