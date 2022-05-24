595aa4cebec1d4b512fca8c4ae372fd595c6969d
hiddy0329 <hiddy19970329@gmail.com>
2022-05-24 16:03:08 +0900

    画像登録用のフォームを実装

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,7 +7,7 @@
         @include('errors')
         <div class="card-header">新規投句</div>
         <div class="card-body">
-            <form method='POST' action="/store">
+            <form method='POST' action="/store" enctype="multipart/form-data">
                 @csrf
                 <input type='hidden' name='user_id' value="{{ $user['id'] }}">
                 <div class="form-group">
@@ -17,6 +17,10 @@
                 <div class="form-group">
                     <label for="description">説明文</label>
                     <textarea name='description' class="form-control"rows="10"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="image">画像登録</label>
+                    <input type="file" class="form-control-file" name='image' id="image">
                 </div>
                 <button type='submit' class="btn btn-primary btn-lg">投句</button>
             </form>
